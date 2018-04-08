9edbf2c7b554b7e1c93d66d1e46989ca53977900
Ben Thomas <ben.thomas001@umb.edu>
2018-04-08 18:38:41 -0400

    update

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/list/<path:directory>'
+ALLOWED_EXTENSIONS = set([])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.debug = True
 source_folder = r"C:\Users\Ben\benproject"
@@ -35,9 +41,24 @@ def list_folderroot():
 def download(filename):
     return send_from_directory(source_folder,filename)
 
-@app.route('/upload',methods=["post"])
-def upload():
-    pass
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    print (file)
 
 
 #Upload files
