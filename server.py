122303cc29ee4a017974f3224c51297e4d75894d
Ben Thomas <ben.thomas001@umb.edu>
2018-04-30 23:27:45 -0400

    mmm

--- server.py
+++ server.py
@@ -1,64 +1,152 @@
+
 import os
-from flask import Flask, render_template, send_from_directory, request, redirect, url_for
+from flask import Flask, flash, render_template, send_from_directory, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/list/<path:directory>'
-ALLOWED_EXTENSIONS = set([])
+
+import hashlib
+salt = "thisissalt"
+
+import mysql.connector
+from mysql.connector import errorcode
+
+
+cnx = mysql.connector.connect(user='root', password='Cundis00!',
+                                host ='127.0.0.1',
+                                database='users_passwords')
+
+def execute_query(sql):
+    cursor = cnx.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+def execute_permission_query(sql):
+    cursor = cnx.cursor()
+    cursor.execute(sql)
+    cursor.close()
+    return result
+
+
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.secret_key = 'admin'
+ALLOWED_EXTENSIONS = set(['jpg', 'mp3','mp4', 'mov' ,'txt', 'doc'])
 
 app.debug = True
 source_folder = r"C:\Users\Ben\benproject"
 
 @app.route('/list/<path:directory>')
 def list_folder(directory):
-    folder_contents = os.listdir(os.path.join(source_folder,directory))
+    if session['logged_in'] == False:
+         return redirect('/login')
+    folder_contents = os.listdir(os.path.join(source_folder, directory))
     print folder_contents
     filelist = []
     folderslist = []
     for i in folder_contents:
-        if os.path.isfile(os.path.join(source_folder,directory,i)) == True:
+        if os.path.isfile(os.path.join(source_folder, directory, i)) == True:
             filelist.append(i)
-        elif os.path.isdir(os.path.join(source_folder,directory,i)) == True:
+        elif os.path.isdir(os.path.join(source_folder, directory, i)) == True:
             folderslist.append(i)
 
-    return render_template("index.html",files=filelist, folders=folderslist)
-
+    return render_template("index.html", files=filelist, folders=folderslist)
 @app.route('/list')
 def list_folderroot():
+    if session['logged_in'] == False:
+         return redirect('/login')
     folder_contents = os.listdir(source_folder)
     filelist = []
     folderslist = []
     for i in folder_contents:
-        if os.path.isfile(os.path.join(source_folder,i)) == True:
+        if os.path.isfile(os.path.join(source_folder, i)) == True:
             filelist.append(i)
-        elif os.path.isdir(os.path.join(source_folder,i)) == True:
+        elif os.path.isdir(os.path.join(source_folder, i)) == True:
             folderslist.append(i)
 
-    return render_template("index.html",files=filelist, folders=folderslist)
+    return render_template("index.html", files=filelist, folders=folderslist)
+
 @app.route('/download/<path:filename>')
 def download(filename):
     return send_from_directory(source_folder,filename)
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    f = filename.split('.')
+    extension = f[-1]
+    if extension in ALLOWED_EXTENSIONS:
+        return True
+    return False
 
-@app.route('/upload', methods=['GET', 'POST'])
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    print (file)
+        print request.files
+        file = request.files['uploadfile']
+        print file
+        file.save(os.path.join(source_folder, file.filename))
+        allowed_file(file.filename)
+    return redirect("/list")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if check_credentials(username,password):
+            session['logged_in'] = True
+            flash('You were successfully logged in')
+            return redirect('/list')
+        return redirect('/login')
+
+def check_credentials(username,password):
+    sql = "select username, password from user_credentials where username = '{}'".format(username)
+    print sql
+    result = execute_query(sql)
+    hp = hashpassword(password)
+    if username == result[0] and hp == result[1]:
+        return True
+    return False
+
+
+def hashpassword(p):
+    h = hashlib.sha256(p + salt)
+    return h.hexdigest()
+
+
+
+
+def select_query(file_path,allowed_users):
+
+
+    result = execute_permission_query(sql)
+
+
+
+
+
+def insert_query():
+
+
+    result = execute_permission_query(sql)
+
+
+
+
+
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session['logged_in'] = False
+    return redirect ('/login')
+
+
+
 
 
 #Upload files
